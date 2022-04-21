a994a35d6bb75526e3e5684180d84718864ecf62
m-cowan <m.cowan13@live.com>
2022-04-22 08:38:27 +1000

    Created additional method stubs and fixed formatting

--- contracts/BetContract.sol
+++ contracts/BetContract.sol
@@ -4,6 +4,8 @@ import "hardhat/console.sol";
 
 contract BetContract {
 
+    struct 
+
     /**
      * Contract initialization.
      */
@@ -14,8 +16,41 @@ contract BetContract {
     /**
      * A function to place bets on a particular sport.
      */
-    function place_bet(address sport, uint256 amount) external {
+    function PlaceBet() {
 
     }
 
+    /**
+     * A function that adds a team to the list of available teams
+     */
+    function AddTeam() {
+
+    }
+
+    /**
+     * A function that adds a fixture.
+     *
+     * @param home the home team
+     * @param away the away team
+     * @param date the date of the match
+     */
+    function AddFixture(string home, string away, string date) {
+
+    }
+
+    /*
+     * A functin that distributes the losers funds between the winners
+     * proportionally based on the winners initial bet amounts 
+     */
+    function DistributeWinnings() {
+
+    }
+
+    /*
+     * Return all funds to players who have bet on this fixture.
+     * To be utilised in the event if a draw or match cancellation
+     */
+    function ReturnFunds() {
+
+    }
 }
