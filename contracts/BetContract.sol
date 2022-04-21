5d62df5a3178075edc60e7d93683027f59fca760
m-cowan <m.cowan13@live.com>
2022-04-22 07:59:12 +1000

    Created initial Betting Smart Contract and method stub

---
+++ contracts/BetContract.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.7.0;
+
+import "hardhat/console.sol";
+
+contract BetContract {
+
+    /**
+     * Contract initialization.
+     */
+    constructor() {
+
+    }
+
+    /**
+     * A function to place bets on a particular sport.
+     */
+    function place_bet(address sport, uint256 amount) external {
+
+    }
+
+}
