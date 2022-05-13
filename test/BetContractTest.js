1e090e9ffee4718fc4b41703811b202d10d3b7f4
Michael Cowan <m.cowan13@live.com>
2022-05-13 21:34:50 +1000

    Merged contractOwnerV3 changes and created unit tests for Fixtures

--- test/BetContractTest.js
+++ test/BetContractTest.js
@@ -2,11 +2,10 @@
 // so feel free to add new ones.
 
 const { expect } = require("chai");
-const { ethers, waffle } = require("hardhat")
+const { ethers, waffle, hardhatArguments } = require("hardhat")
 
 describe("BetContract contract", function () {
 
-    let Token;
     let betContract;
     let owner;
     let addr1;
@@ -37,6 +36,52 @@ describe("BetContract contract", function () {
         it("Should test for account balances", async function() {
         let balance = await prov.getBalance(owner.address)
         console.log("Balance", balance);
+        });
+
+        it("Should set the correct owner", async function(){
+            expect(await betContract.owner()).to.equal(owner.address);
+        });
+    });
+
+    describe("Fixture", function () {
+
+        it("Should be able to create multiple new Fixtures", async function() {
+            //Ensure Fixture Count is initialised
+            expect(await betContract.getFixtureCount()).to.equal(0);
+            
+            //Test adding of first Fixture
+            await betContract.addFixture("Red Sox","White Sox", "14 May 22");
+            expect((await betContract.getFixture(0)).fixId).to.equal(0);
+            expect((await betContract.getFixture(0)).home).to.equal("Red Sox");
+            expect((await betContract.getFixture(0)).away).to.equal("White Sox");
+            expect((await betContract.getFixture(0)).date).to.equal("14 May 22");
+            expect((await betContract.getFixture(0)).active).to.equal(true);
+            expect((await betContract.getFixture(0)).payedOut).to.equal(false);
+            expect((await betContract.getFixture(0)).invalidated).to.equal(false);
+            
+            //Ensure Fixture Count is incremented
+            expect(await betContract.getFixtureCount()).to.equal(1);
+            
+            //Test adding of second Fixture
+            await betContract.addFixture("Angels", "Dodgers", "15 May 22");
+            expect((await betContract.getFixture(1)).fixId).to.equal(1);
+            expect((await betContract.getFixture(1)).home).to.equal("Angels");
+            expect((await betContract.getFixture(1)).away).to.equal("Dodgers");
+            expect((await betContract.getFixture(1)).date).to.equal("15 May 22");
+            expect((await betContract.getFixture(1)).active).to.equal(true);
+            expect((await betContract.getFixture(1)).payedOut).to.equal(false);
+            expect((await betContract.getFixture(1)).invalidated).to.equal(false);
+            
+            //Ensure Fixture Count is incremented
+            expect(await betContract.getFixtureCount()).to.equal(2);
+
+            //Ensure list of fixture IDs are updated
+            expect((await betContract.getFixtures())[0]).to.equal(0);
+            expect((await betContract.getFixtures())[1]).to.equal(1);
+        });
+
+        it("Should not be able to create a Fixture as a non-owner", async function() {
+            await expect(betContract.connect(addr1).addFixture("Red Sox","White Sox", "14 May 22")).to.be.revertedWith("Only UQ Sports Administration can add Fixtures");
         });
     });
 
